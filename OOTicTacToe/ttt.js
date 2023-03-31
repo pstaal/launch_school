bcd446dce5baa097df49fe70ae4633c68c57a0d1
peter staal <peterstaal@MBP-van-peter.home>
2023-03-31 13:50:09 +0200

    complete object oriented tictactoe game

--- OOTicTacToe/ttt.js
+++ OOTicTacToe/ttt.js
@@ -16,6 +16,14 @@ class Square {
   setMarker(marker) {
     this.marker = marker;
   }
+
+  isUnused() {
+    return this.marker === Square.UNUSED_SQUARE;
+  }
+
+  getMarker() {
+    return this.marker;
+  }
 }
 
 class Board {
@@ -45,11 +53,29 @@ class Board {
   markSquareAt(key, marker) {
     this.squares[key].setMarker(marker);
   }
-}
 
-class Row {
-  constructor() {
-    // We need some way to identify a row of 3 squares
+  isFull() {
+    return this.unusedSquares().length === 0;
+  }
+
+  unusedSquares() {
+    let keys = Object.keys(this.squares);
+    return keys.filter(key => this.squares[key].isUnused());
+  }
+
+  countMarkersFor(player, keys) {
+    let markers = keys.filter(key => {
+      return this.squares[key].getMarker() === player.getMarker();
+    });
+
+    return markers.length;
+  }
+
+  displayWithClear() {
+    console.clear();
+    console.log("");
+    console.log("");
+    this.display();
   }
 }
 
@@ -60,11 +86,6 @@ class Player {
 
   getMarker() {
     return this.marker;
-  }
-
-  play() {
-    // We need a way for each player to play the game.
-    // Do we need access to the board?
   }
 }
 
@@ -81,6 +102,17 @@ class Computer extends Player {
 }
 
 class TTTGame {
+  static POSSIBLE_WINNING_ROWS = [
+    [ "1", "2", "3" ],            // top row of board
+    [ "4", "5", "6" ],            // center row of board
+    [ "7", "8", "9" ],            // bottom row of board
+    [ "1", "4", "7" ],            // left column of board
+    [ "2", "5", "8" ],            // middle column of board
+    [ "3", "6", "9" ],            // right column of board
+    [ "1", "5", "9" ],            // diagonal: top-left to bottom-right
+    [ "3", "5", "7" ],            // diagonal: bottom-left to top-right
+  ];
+
   constructor() {
     this.board = new Board();
     this.human = new Human();
@@ -90,25 +122,26 @@ class TTTGame {
   play() {
     this.displayWelcomeMessage();
 
+    this.board.display();
     while (true) {
-      this.board.display();
-
       this.humanMoves();
-      this.board.display(); // so we can see human's move
       if (this.gameOver()) break;
 
       this.computerMoves();
-      this.board.display(); // so we can see the computer's move
       if (this.gameOver()) break;
-      break; // <= execute loop only once for now
+
+      this.board.displayWithClear();
     }
 
+    this.board.displayWithClear();
     this.displayResults();
     this.displayGoodbyeMessage();
   }
 
   displayWelcomeMessage() {
+    console.clear();
     console.log("Welcome to Tic Tac Toe!");
+    console.log("");
   }
 
   displayGoodbyeMessage() {
@@ -116,19 +149,24 @@ class TTTGame {
   }
 
   displayResults() {
-    // show the results of this game (win, lose, tie)
+    if (this.isWinner(this.human)) {
+      console.log("You won! Congratulations!");
+    } else if (this.isWinner(this.computer)) {
+      console.log("I won! I won! Take that, human!");
+    } else {
+      console.log("A tie game. How boring.");
+    }
   }
 
   humanMoves() {
     let choice;
 
     while (true) {
-      choice = readline.question("Choose a square between 1 and 9: ");
+      let validChoices = this.board.unusedSquares();
+      const prompt = `Choose a square (${validChoices.join(", ")}): `;
+      choice = readline.question(prompt);
 
-      let integerValue = parseInt(choice, 10);
-      if (integerValue >= 1 && integerValue <= 9) {
-        break;
-      }
+      if (validChoices.includes(choice)) break;
 
       console.log("Sorry, that's not a valid choice.");
       console.log("");
@@ -138,12 +176,28 @@ class TTTGame {
   }
 
   computerMoves() {
-    let choice = Math.floor((9 * Math.random()) + 1);
+    let validChoices = this.board.unusedSquares();
+    let choice;
+
+    do {
+      choice = Math.floor((9 * Math.random()) + 1).toString();
+    } while (!validChoices.includes(choice));
+
     this.board.markSquareAt(choice, this.computer.getMarker());
   }
 
   gameOver() {
-    return false;
+    return this.board.isFull() || this.someoneWon();
+  }
+
+  someoneWon() {
+    return this.isWinner(this.human) || this.isWinner(this.computer);
+  }
+
+  isWinner(player) {
+    return TTTGame.POSSIBLE_WINNING_ROWS.some(row => {
+      return this.board.countMarkersFor(player, row) === 3;
+    });
   }
 }
 
